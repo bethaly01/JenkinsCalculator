b9b3b6dc06fc66ead87ada6138eb32f66b3a4d87
Bethaly01 <btenang2@byu.edu>
2022-02-19 21:02:03 -0700

    update createUniqueID second time

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,3 +1,5 @@
+import javafx.util.Builder;
+
 import java.util.UUID;
 
 class Calculator {
@@ -68,7 +70,10 @@ class Calculator {
     if you run this function twice with the same String input, it must return 2 unique String IDs
      */
     String createUniqueID(String n){
-        return UUID.randomUUID().fromString(n).toString();
+        StringBuilder newString = new StringBuilder(n);
+        String random = UUID.randomUUID().toString();
+        newString.append(random);
+        return newString.toString();
     }
 
 
